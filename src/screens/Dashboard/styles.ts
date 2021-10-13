df62dca660ab99f2e26e2809db470e71e1010e2f
AndreCosta101 <andre.costa101@gmail.com>
2021-10-13 17:21:37 -0300

    Posicionando os cartões em destaque

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 
 export const Container = styled.View`
@@ -14,8 +15,18 @@ export const Header = styled.View`
 
   background-color: ${({ theme }) => theme.colors.primary};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -47,15 +58,6 @@ export const UserName = styled.Text`
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
 
-export const UserWrapper = styled.View`
-  width: 100%;
-  padding: 0 24px;
-
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
@@ -66,5 +68,8 @@ export const HighlightCardsWrapper = styled.ScrollView.attrs({
   showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingHorizontal: 24 }
 })`
-  
+  width: 100%;
+
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
