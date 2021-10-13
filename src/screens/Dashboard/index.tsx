e22f6299c0c29a52722e9cc08c58fa6f7b1ea906
AndreCosta101 <andre.costa101@gmail.com>
2021-10-13 16:09:22 -0300

    Utilizando proporcoes em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+  UserWrapper
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/48858873?v=4' }}
+            />
+            <User>
+              <UserGreetings>Olá, </UserGreetings>
+              <UserName>André</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
