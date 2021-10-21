b13159afd076f88363ef185fec0d5e51b647059a
AndreCosta101 <andre.costa101@gmail.com>
2021-10-21 12:21:26 -0300

    erro no response do GoogleSignIn

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -24,6 +24,13 @@ interface IAuthContextData {
   signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 function AuthProvider({ children }: AuthProviderProps) {
   const user = {
     id: '1232313',
@@ -40,8 +47,16 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl })
-      console.log(response)
+      const { type, params } = await AuthSession
+        .startAsync({ authUrl }) as AuthorizationResponse;
+      console.log(type)
+      if (type === 'success') {
+        const response = await fetch(`https://googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        console.log(userInfo)
+      }
+
 
     } catch (error: any) {
       throw new Error(error)
